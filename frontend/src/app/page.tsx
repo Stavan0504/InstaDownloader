59933ab4f01859ae92495e04efc2bd45b10dfdfb
bhargav <142798009+Stavan0504@users.noreply.github.com>
2025-05-06 16:15:10 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
     try {
       console.log(url)
-      const response = await axios.post(`https://instadownloader-backend.up.railway.app/get-video`, {
+      const response = await axios.post(`https://instadownloader-backend-production.up.railway.app`, {
         url: url,
         headers: {
           "Content-Type": "application/json",
